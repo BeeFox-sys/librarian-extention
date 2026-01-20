dc014b41ad85e49a39914f7228f66d6898c1adbf
bee <bee@beefox.xyz>
2026-01-20 20:27:48 +1100

    have options open on missing data

--- background.js
+++ background.js
@@ -7,13 +7,8 @@ async function savePage(event) {
     let options = await browser.storage.sync.get();
 
 
-    if (!options.librarian_address) {
-        let alertWindow = 'alert("No Librarian Address Provided!")';
-        browser.tabs.executeScript({code : alertWindow});
-    }
-    if (!options.token) {
-        let alertWindow = 'alert("No Token Provided!")';
-        browser.tabs.executeScript({code : alertWindow});
+    if (!options.librarian_address || !options.token) {
+        browser.runtime.openOptionsPage()
     }
     
     let restab = await browser.tabs.create({
@@ -26,7 +21,6 @@ async function savePage(event) {
         let tab = await browser.tabs.get(event)
         // This seems to be the best way to check the tab has *actually* opened and loaded
         if (tab.status = "complete" && tab.favIconUrl) {
-            console.log(tab)
             browser.tabs.remove(event)
         }
     }, 
